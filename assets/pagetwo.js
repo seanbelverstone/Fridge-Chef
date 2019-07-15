c87eb4be8c20bb6aaf772c6415e038e1f746d62d
Sean Belverstone <seanbelverstone@gmail.com>
2019-07-14 20:55:26 -0500

    Created showArray function, which will be a function that will work throughtout each food group click. It creates a label and an input which will contain the corresponding text.

--- assets/pagetwo.js
+++ assets/pagetwo.js
@@ -1,20 +1,44 @@
-var proteinsArray = ['Bacon', 'Beef', 'Pork', 'Chicken', 'Lamb Meat', 'Fish', 'Shrimp', 'Turkey'];
-var grainsArray = ['Rice', 'Brown Rice', 'Wheat', 'Oat', 'Quinoa', 'Pasta', 'Almonds', 'Walnuts'];
-var fruitAndVegArray = ['Lemons', 'Limes', 'Apple', 'Tomato', 'Celery', 'Leek', 'Onion', 'Potato', 'Blueberries', 'Strawberries', 'Banana', 'Cabbage', 'Lettuce', 'Mushrooms', 'Avocado', 'Carrots', 'Bell Peppers', 'Broccoli', 'Cucumber', 'Jalapeño', 'Garlic'];
-var dairyArray = ['Milk', 'Cheese', 'Yoghurt', 'Heavy Cream', 'Butter', 'Eggs'];
-var condimentsArray = ['Mustard', 'Mayo', 'Ketchup', 'Olive Oil', 'Vinegar', 'Balsamic', 'Honey', 'Soy Sauce', 'Sesame Oil'];
+// Initial Arrays for Page Two
 
+var ingredients = {
+    proteinsArray: ['Bacon', 'Beef', 'Chicken', 'Fish', 'Pork',  'Lamb',  'Shrimp', 'Turkey'],
+    grainsArray: ['Almonds', 'Brown Rice', 'Oat', 'Pasta', 'Quinoa', 'Rice', 'Walnuts', 'Wheat'],
+    fruitAndVegArray: ['Lemons', 'Limes', 'Apple', 'Tomato', 'Celery', 'Leek', 'Onion', 'Potato', 'Blueberries', 'Strawberries', 'Grapes', 'Banana', 'Cabbage', 'Lettuce', 'Mushrooms', 'Avocado', 'Carrots', 'Bell Peppers', 'Broccoli', 'Cucumber', 'Jalapeño', 'Garlic'],
+    dairyArray: ['Milk', 'Cheese', 'Yoghurt', 'Heavy Cream', 'Butter', 'Eggs'],
+    condimentsArray: ['Mustard', 'Mayo', 'Ketchup', 'Olive Oil', 'Vinegar', 'Balsamic', 'Honey', 'Soy Sauce', 'Sesame Oil']
+}
 
+function showArray() {
+    for (var i = 0; i < Array.length; i++) {
+        
+        var checkbox = $("<label>");
+        var input = $("<input>").attr("type", "checkbox");
+        checkbox.append(input);
+        var ingredientText = $("<span>");
+        checkbox.append(ingredientText);
+        $("#ingredientSection").append(checkbox);
+    }
+}
 
-$("#fridge").on("click", function() {
-
-})
+/*      <p>
+            <label>
+            <input type="checkbox" class="filled-in" checked="checked" />
+            <span>Filled in</span>
+            </label>
+        </p>*/
 
 $("#protein").on("click", function() {
+    $("#foodGroupTitle").text("Proteins");
+    // ingredientText.val(ingredients.proteinsArray[i])
+    showArray();
 
 })
 
+// var toDoClose = $("<button>");
 
+// toDoClose.attr("data-to-do", toDoCount);
+// toDoClose.addClass("checkbox");
+// toDoClose.text("✓");
 
 
 $(document).on("click", ".submit", function() {
